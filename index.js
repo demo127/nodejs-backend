c8ce1005bd011231e99d73133b701a30f951ad17
demo127 <32142662+demo127@users.noreply.github.com>
2024-02-06 01:10:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    let op = JSON.stringify(req.headers)
+    let op = JSON.stringify(request.headers)
     response.end("Backend headers: " + op);
     // res.render('index', { title: op })
     
